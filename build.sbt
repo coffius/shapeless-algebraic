523949a0dcbad1f96686417145f73a2278ba35a1
Aleksei Shamenev <ashamenev@evolutiongaming.com>
2018-03-26 11:08:34 +0300

    add more examples

--- build.sbt
+++ build.sbt
@@ -4,7 +4,8 @@ version := "1.0"
 
 scalaVersion := "2.12.5"
 
-scalacOptions += "-Ypartial-unification"
+
+scalacOptions += "-Xfatal-warnings"
 
 libraryDependencies += "com.chuusai"                %% "shapeless"            % "2.3.3"
 libraryDependencies += "org.scalatest"              %% "scalatest"            % "3.0.4"     % Test
--- src/main/scala/io/koff/shapeless_algebraic/SealedTrait.scala
+++ src/main/scala/io/koff/shapeless_algebraic/SealedTrait.scala
@@ -1,7 +1,15 @@
 package io.koff.shapeless_algebraic
 
+import io.koff.shapeless_algebraic.SealedTrait.BasicProtocol._
+import io.koff.shapeless_algebraic.SealedTrait.ExtendedProtocol.{GetAllUsers, _}
+import io.koff.shapeless_algebraic.SealedTrait.WrapperExtension.Wrapper.Evidence
+import shapeless._
+
 object SealedTrait {
-  object Protocol {
+  //TODO: What is ADT?
+  //TODO: Where/Why are ADT's used?
+
+  object BasicProtocol {
     sealed trait Request
     case class CreateUser(name: String)                   extends Request
     case class ReadUserInfo(userId: Int)                  extends Request
@@ -14,13 +22,99 @@ object SealedTrait {
     case class UserUpdated(userId: Int, name: String) extends Response
     case class UserDeleted(userId: Int)               extends Response
 
+
     //TODO: Exhaustiveness Checking for sealed traits
     //TODO: compiler flag warn-2-error
-    // Problem - how to extend protocol without access to code of a sealed trait
-    // Solution - Wrapper + Evidence
-    // Problem - how to make a compiler to check Exhaustiveness of extended protocol(Wrapper + Evidence)
-    // Solution - shapeless Coproduct + Poly
-    // Addition#1 - merging Coproducts
-    // Addition#2 - merging Polys
+    private def exhaustivePatternMatch(request: Request) = {
+      request match {
+        case CreateUser(name)         => ???
+        case ReadUserInfo(id)         => ???
+        case UpdateUserInfo(id, name) => ???
+        case DeleteUser(id)           => ???
+      }
+    }
+
+    // with `-Xfatal-warnings` non-exhaustive matches will not compile
+    private def nonExhaustivePatternMatch(response: Response) = {
+      response match {
+        case res: UserCreated  => ???
+        case res: UserInfo     => ???
+        case res: UserUpdated  => ???
+        case res: UserDeleted  => ???
+      }
+    }
   }
+
+  // Problem - how to extend protocol without access to code of a sealed trait
+  // Solution - Wrapper + Evidence
+  object ExtendedProtocol {
+    sealed trait AdvancedRequest
+    case object GetAllUsers     extends AdvancedRequest
+    case object DeleteAllUsers  extends AdvancedRequest
+
+    sealed trait AdvancedResponse
+    case class AllUsers(names: Seq[String]) extends AdvancedResponse
+    case class DeletedUsers(ids: Seq[Int])  extends AdvancedResponse
+  }
+
+  object WrapperExtension {
+    case class Wrapper[T: Evidence](value: T)
+    object Wrapper {
+      sealed trait Evidence[-T]
+      implicit object RequestEvidence extends Evidence[Request]
+      implicit object AdvancedRequestEvidence extends Evidence[AdvancedRequest]
+    }
+
+    val withBasicRequest = Wrapper(CreateUser("test-user"))
+    val withAdvancedRequest = Wrapper(GetAllUsers)
+    // does not compile with `withInvalidValue` uncommented
+    //val withInvalidValue = Wrapper(DeletedUsers(Seq(1, 2)))
+
+    // Problem - how to make a compiler to check Exhaustiveness of the extended protocol(Wrapper + Evidence)
+    private def patternMatch[T: Evidence](wrapper: Wrapper[T]) = {
+      // compiles with several matches missed - no warns or errors :(
+      wrapper.value match {
+        case CreateUser(name)         => ???
+        case ReadUserInfo(id)         => ???
+      }
+    }
+  }
+
+  // Solution - shapeless Coproduct + Poly
+  object UsingPolys {
+    // Ad Hoc ADT is built from arbitrary types
+    type ReadRequests   = ReadUserInfo :+: GetAllUsers.type :+: CNil
+    type WriteRequests  = CreateUser :+: UpdateUserInfo :+: DeleteUser :+: DeleteAllUsers.type :+: CNil
+
+    object read extends Poly1 {
+      implicit val readUserInfo = at[ReadUserInfo]    (_ => true)
+      implicit val getAllUsers  = at[GetAllUsers.type](_ => 1)
+    }
+
+    object write extends Poly1 {
+      implicit val createUser     = at[CreateUser]          (_ => true)
+      implicit val updateUserInfo = at[UpdateUserInfo]      (_ => 1)
+      implicit val deleteUser     = at[DeleteUser]          (_ => "2")
+      implicit val deleteAllUsers = at[DeleteAllUsers.type] (_ => List(3))
+    }
+
+    private def readMatch(request: ReadRequests) = {
+      val result = request.map(read)
+    }
+
+    private def writeMatch(request: WriteRequests) = {
+      val result = request.map(write)
+    }
+  }
+
+  // Addition#1 coproducts {}
+  // - merging Coproducts
+  object MergingCoproducts {
+    /*
+    val joined = Adjoin[ADT1.CR :+: ADT2.UD]
+  type CRUD_combined = joined.Out
+     */
+  }
+  // Addition#2 - merging Polys
+  // Addition#3 - ADT's in other languages(Idris)
 }
